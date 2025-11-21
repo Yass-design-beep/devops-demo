0d5b5d0c00ac726009ed8616107d8c8a58a5f7e2
Yass-design-beep <yacineS0424@gmail.com>
2025-11-21 12:50:13 +0000

    fix Ajout pipeline CI

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/products', function () {
     return redirect()->route('products.index');
 });
 // page de confirmation avant suppression
